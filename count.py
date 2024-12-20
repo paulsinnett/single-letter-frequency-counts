20c48c1af9eecb9b21ccfe391b4d5f9fd347465d
Paul Sinnett <paul.sinnett@gmail.com>
2024-12-20 00:42:44 +0000

    Update count.py

--- count.py
+++ count.py
@@ -1,10 +1,13 @@
 import argparse
+import math
 import random
 import csv
+from statistics import mean, stdev
 import unicodedata
+from collections import Counter
 
 parser = argparse.ArgumentParser(prog='count.py', description='counts the letter frequency for letter positions within a text')
-parser.add_argument('--source', help='source documents directory', default='OANC-GrAF')
+parser.add_argument('--source', help='source of text: oanc, norvig, oanc-list', default='oanc')
 parser.add_argument('--source-count', help='sources to sample', default=100)
 parser.add_argument('--word-sample-count', help='number of words to collect from a source', default=200)
 parser.add_argument('--bias-to-front', help='bias the random starting point towards the front of the article', default=True)
@@ -12,7 +15,8 @@ parser.add_argument('--word-list', help='restrict acceptable words Scrabble, com
 parser.add_argument('--written-only', help='only process written texts', action='store_true')
 parser.add_argument('--ignore-punctuation', help='ignore punctuation within words', default=True)
 parser.add_argument('--strip-accents', help='strip accents from letters in a word', default=True)
-parser.add_argument('--output', help='output cvs filename', default='output')
+parser.add_argument('--output', help='output csv filename', default=None)
+parser.add_argument('--stat-table', help='filename for a csv table of K counts', default=None)
 
 args = parser.parse_args()
 
@@ -70,12 +74,8 @@ def convert_to_types(sample_list):
 	for word in sample_list:
 		length = len(word)
 		if length not in types:
-			types[length] = {}
-		
-		if word in types[length]:
-			types[length][word] += 1
-		else:
-			types[length][word] = 1
+			types[length] = Counter()
+		types[length][word] += 1
 	return types
 
 def sample_words(file, dictionary):
@@ -105,18 +105,9 @@ def sample_words(file, dictionary):
 				pos += 1
 	return sample_list
 
-def collect_sample():
-	valid_words = None
-	match args.word_list:
-		case 'Scrabble':
-			print('loading Scrabble dictionary')
-			valid_words = load_valid_words('Scrabble-dictionary.txt')
-		case 'common':
-			print('loading common words dictionary')
-			valid_words = load_valid_words('google-books-common-words.txt')
-		case _:
-			valid_words = None
-	files = files_in_directory(args.source)
+
+def collect_sample(valid_words):
+	files = files_in_directory('OANC-GrAF')
 	sources = list(filter(lambda file: filter_file(file), files))
 	random.shuffle(sources)
 	samples = 0
@@ -133,17 +124,25 @@ def collect_sample():
 	if samples < int(args.source_count):
 		print(f'not enough sources to collect {args.word_sample_count} words from {args.source_count} sources, only got {samples}')
 
-	return convert_to_types(collection)
+	return collection
 
-def letter_frequency_count(headers, types):
+def create_frequency_table():
 	letter_position_count = {}
 	for letter in alphabet:
-		letter_position_count[letter] = {}
-		for col in headers:
-			letter_position_count[letter][col] = 0
+		letter_position_count[letter] = Counter()
+	return letter_position_count
 
+def create_frequency_distribution_table(headers):
+	letter_position_distribution = {}
+	for letter in alphabet:
+		letter_position_distribution[letter] = {}
+		for col in headers:
+			letter_position_distribution[letter][col] = []
+	return letter_position_distribution
+
+def letter_frequency_count(types):
+	letter_position_count = create_frequency_table()
 	for length in range(3, 8):
-		type_count = len(types[length])
 		token_count = 0
 		for word, tokens in types[length].items():
 			token_count += tokens
@@ -151,15 +150,14 @@ def letter_frequency_count(headers, types):
 				col = column(length, p)
 				letter_position_count[word[p]][col] += tokens
 				letter_position_count[word[p]]['T'] += tokens
-		print(f'{length} letter words, {token_count} tokens, {type_count} types;')
 
 	return letter_position_count
 
 def display_count(count):
-	return f'{count}' if count > 0 else ''
+	return f'{count}' if count != 0 else ''
 
-def output_table(headers, letter_position_count):
-	with open(f'{args.output}.csv', 'w', newline='') as file:
+def output_table(filename, headers, letter_position_count):
+	with open(filename, 'w', newline='') as file:
 		writer = csv.writer(file)
 		writer.writerow(headers)
 		for letter in alphabet:
@@ -171,14 +169,150 @@ def output_table(headers, letter_position_count):
 			row.append(display_count(letter_position_count[letter]['T']))
 			writer.writerow(row)
 
-headers = create_headers()
-types = collect_sample()
-letter_position_count = letter_frequency_count(headers, types)
-output_table(headers, letter_position_count)
-sum_1st = 0
-sum_3rd = 0
-for length in range(3, 8):
-	sum_1st += letter_position_count['K'][column(length, 0)]
-	sum_3rd += letter_position_count['K'][column(length, 2)]
+def load_types_and_tokens(valid_words):
+	counter = Counter()
+	if args.source == 'norvig':
+		with open ('google-books-common-words.txt', 'r') as file:
+			for line in file:
+				word, frequency = line.split()
+				if len(word) >= 3 and len(word) <= 7:
+					if valid_words == None or valid_word(word, valid_words):
+						counter[word] = int(frequency)
+	else:
+		files = files_in_directory('OANC-GrAF')
+		sources = list(filter(lambda file: filter_file(file), files))
+		for file in sources:
+			sample_list = sample_words(file, valid_words)
+			counter.update(sample_list)
 
-print(f'Ratio of words with K as 3rd letter {sum_3rd} : words beginning with K {sum_1st} is {sum_3rd / sum_1st:.2f} : 1')
+	return counter
+
+def generate_table(valid_words, counter, headers):
+	if counter == None:
+		word_sample = collect_sample(valid_words)
+	else:
+		word_sample = random.choices(list(counter.keys()), list(counter.values()), k=int(args.word_sample_count) * int(args.source_count))
+
+	types = convert_to_types(word_sample)
+	letter_position_count = letter_frequency_count(types)
+	if args.output:
+		output_table(f'{args.output}.csv', headers, letter_position_count)
+	return letter_position_count
+
+def open_table(filename, headers):
+	letter_position_count = create_frequency_table()
+	with open(filename, newline='') as file:
+		reader = csv.reader(file)
+		for row in reader:
+			letter = row[0]
+			if letter != '':
+				for i in range(1, len(row)):
+					if i < len(headers):
+						letter_position_count[letter][headers[i]] = 0 if row[i] == '' else int(row[i])
+	return letter_position_count
+
+valid_words = None
+headers = create_headers()
+match args.word_list:
+	case 'Scrabble':
+		valid_words = load_valid_words('Scrabble-dictionary.txt')
+	case 'common':
+		valid_words = load_valid_words('google-books-common-words.txt')
+	case _:
+		valid_words = None
+counter = None
+if args.source == 'norvig' or args.source == 'oanc-list':
+	counter = load_types_and_tokens(valid_words)
+if args.output:
+	generate_table(valid_words, counter, headers)
+elif args.stat_table:
+	stat_table = open_table(f'{args.stat_table}.csv', headers)
+	letter_position_distribution = create_frequency_distribution_table(headers)
+	letter_position_z_score = create_frequency_table()
+	for trial in range (100):
+		letter_position_count = generate_table(valid_words, counter, headers)
+		for letter in alphabet:
+			for col in headers:
+				letter_position_distribution[letter][col].append(letter_position_count[letter][col])
+	stddev4 = 0
+	total = 0
+	for letter in alphabet:
+		for col in headers:
+			average = mean(letter_position_distribution[letter][col])
+			stddev = stdev(letter_position_distribution[letter][col])
+			z = 0 if stddev == 0 else (stat_table[letter][col] - average) / stddev
+			letter_position_z_score[letter][col] = z
+			if abs(z) < 2:
+				stddev4 += 1
+			total += 1
+	print(f'{int(stddev4 * 100 / total)}% are less than 2 standard deviations from the mean')
+	output_table(f'{args.stat_table}-z.csv', headers, letter_position_z_score)
+
+
+# def calculate_k_first():
+# 	letter_position_count = generate_table()
+# 	sum_k_1st = 0
+# 	for length in range(3, 8):
+# 		sum_k_1st += letter_position_count['K'][column(length, 0)]
+# 	return sum_k_1st
+
+# for i in range(10):
+# 	trials = []
+# 	for i in range(100):
+# 		trials.append(calculate_k_first())
+# 	z = (152 - mean(trials)) / stdev(trials)
+# 	root2 = 2**0.5
+# 	probability = 0.5 * (1 + math.erf(z / root2))
+# 	print(f'Probability of finding 152 or less words beginning with K in 20,000 is approximately {round(probability * 100, sigfigs=1)}%')
+
+# def generate_letter_position_count_array(headers):
+# 	letter_position_count_array = {}
+# 	for letter in alphabet:
+# 		letter_position_count_array[letter] = {}
+# 		for col in headers:
+# 			letter_position_count_array[letter][col] = []
+# 	return letter_position_count_array
+
+# def calculate_z_score(letter_position_count_array, headers):
+# 	letter_position_z_score = create_frequency_table(headers)
+# 	with open('run2.csv', newline='') as file:
+# 		reader = csv.reader(file)
+# 		for row in reader:
+# 			letter = row[0]
+# 			if letter in letter_position_count_array:
+# 				for i in range(1, len(row)):
+# 					if i < len(headers):
+# 						x = int(0 if row[i] == '' else row[i])
+# 						#x = letter_position_count_array[letter][headers[i]][0]
+# 						mu = mean(letter_position_count_array[letter][headers[i]])
+# 						sigma = math.sqrt(variance(letter_position_count_array[letter][headers[i]], mu))
+# 						z = 0 if sigma == 0 else (x - mu) / sigma
+# 						letter_position_z_score[letter][headers[i]] = f'{z:.2f}' if abs(z) > 2 else '' # {mu}+/-{sigma*2:.2f}'
+# 	return letter_position_z_score
+
+
+# headers = create_headers()
+# letter_position_count_array = generate_letter_position_count_array(headers)
+# for i in range(100):
+# 	letter_position_count = generate_table(headers)
+# 	for letter in alphabet:
+# 		for col in headers:
+# 			letter_position_count_array[letter][col].append(letter_position_count[letter][col])
+# letter_position_z_score = calculate_z_score(letter_position_count_array, headers)
+# if args.output:
+# 	output_table(headers, letter_position_z_score)
+
+# with open('MayznerTresselt1965.csv', newline='') as file:
+# 	reader = csv.reader(file)
+# 	for row in reader:
+# 		letter = row[0].lower()
+# 		if letter in letter_position_count:
+# 			for i in range(1, len(row)):
+# 				if i < len(headers):
+# 					#x = int(0 if row[i] == '' else row[i])
+# 					x = letter_position_count[letter][headers[i]][0]
+# 					mu = mean(letter_position_count[letter][headers[i]])
+# 					sigma = sqrt(variance(letter_position_count[letter][headers[i]], mu))
+# 					z = 0 if sigma == 0 else (x - mu) / sigma
+# 					cv = 0 if mu == 0 else sigma / mu
+# 					letter_position_count[letter][headers[i]] = abs(z)
